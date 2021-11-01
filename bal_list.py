4a1cdc83d6632246fff610dd3c8d71a366cd87cc
chris0711 <chris1986.711@gmail.com>
2021-11-01 17:57:51 +0800

    最後一個排序有問題，最多最少的不好def

--- bal_list.py
+++ bal_list.py
@@ -1,92 +1,118 @@
-balls = ["blue", "yellow", "red", "red", "orange", "blue", "yellow"]
+from typing import List
 
-print ("this is ball list:",balls)
-
-print()
 #有幾種顏色
 
-n_colors_balls = len(set(balls))
+def n_colors(balls: List) -> int:
+    """"This function counts balls"""
+    return len(set(balls))
 
-print("How many colors in balls:",n_colors_balls)
+#列出所有顏色
 
-print ("colors in balls",set(balls))
-
-print()
+def list_colors(balls:list)->list:
+    """this function list balls color"""
+    return set(balls)
 
 #有幾顆球
 
-n_balls = len(balls)
-
-print ("How many balls :",n_balls)
-
-print()
+def count_balls(balls:list)->int:
+   """this function count ball number"""
+   return len(balls)
 
 #每一種顏色有幾個
 
-n_blue_ball = balls.count("blue")
-n_yellow_ball = balls.count("yellow")
-n_red_ball = balls.count("red")
-n_orange_ball = balls.count("orange")
+def blue_ball_n(balls:list)->int:
+   """this function count blue ball"""
+   return balls.count("blue")
 
-print( "How many blue balls:",n_blue_ball )
-print( "How many yellow balls:",n_yellow_ball )
-print( "How many red balls:", n_red_ball)
-print( "How many orange balls:", n_orange_ball)
-print()
+def yellow_ball_n(balls:list)->int:
+    """this function count yellow ball"""
+    return balls.count("yellow")
+
+def red_ball_n(balls:list)->int:
+    """this function count red ball"""
+    return balls.count("red")
+
+def orange_ball_n(balls:list)->int:
+    """this function count orange ball"""
+    return balls.count("orange")
 
 #最多顏色的球是什麼色
 
-n_balls = [n_blue_ball,n_yellow_ball,n_red_ball,n_orange_ball]
+def max_color(n_balls:dict)->dict:
+   return max(n_balls, key=n_balls.get)
 
-max_n_ball=[]
-
-max_n_ball_color = max(n_balls)
-
-if n_blue_ball == max_n_ball_color:
-   max_n_ball.append("blue")
-if n_yellow_ball == max_n_ball_color:
-   max_n_ball.append("yellow")
-if n_red_ball == max_n_ball_color:
-   max_n_ball.append("red")
-if n_orange_ball == max_n_ball_color:
-   max_n_ball.append("orange")
-print ("maximun number ball color:",max_n_ball)   
-
-print()
 #那個顏色的球最少
 
-min_n_ball=[]
+def min_color(n_balls:dict)->dict:
+   return min(n_balls, key=n_balls.get)
 
-min_n_ball_color = min(n_balls)
-
-if n_blue_ball == min_n_ball_color:
-   min_n_ball.append("blue")
-if n_yellow_ball == min_n_ball_color:
-   min_n_ball.append("yellow")
-if n_red_ball == min_n_ball_color:
-   min_n_ball.append("red")
-if n_orange_ball == min_n_ball_color:
-   min_n_ball.append("orange")
-print ("minimun number ball color:",min_n_ball)
-
-print()
 #平均每個顏色有幾顆球
 
-print ("avg ball in color:",sum(n_balls)/len(n_balls))
-
-print()
 #打印每個顏色及其數量
 
-color_and_number = [("blue",n_blue_ball),("orange",n_orange_ball),("red",n_red_ball),("yellow",n_yellow_ball)]
+#打印出顏色數量由大到小 
 
-print ("color and how many:",color_and_number)
 
-print()
-#打印出顏色數量由大到小
+if __name__ == "__main__":
+   balls = ["blue", "yellow", "red", "red", "orange", "blue", "yellow"]
+   
+   print("所有的球:",balls)
+   
+   print("有幾種顏色",n_colors(balls))
+   
+   print("列出所有顏色",list_colors(balls))
+  
+   print("有幾顆球",count_balls(balls))
 
-def takeSecond(elem):
-    return elem[1]
- 
-color_and_number.sort(reverse=True,key=takeSecond)
+   print("有幾顆藍色的球",blue_ball_n(balls))
 
-print ("由大到小排列:",color_and_number)
+   print("有幾顆黃色的球",yellow_ball_n(balls)) 
+
+   print("有幾顆紅色的球",red_ball_n(balls))
+
+   print("有幾顆橘色的球",orange_ball_n(balls)) 
+
+#嘗試用Dictionary解決
+   
+   n_balls = {"blue":blue_ball_n(balls),"yellow":yellow_ball_n(balls),"red":red_ball_n(balls),"orange":orange_ball_n(balls)}
+   
+   max_key = max_color(n_balls)
+
+   max_balls_number = n_balls[max_key]
+
+   min_key = min_color(n_balls)
+   
+   min_balls_number = n_balls[min_key]
+   
+   max_in_n_balls=[]
+
+   min_in_n_balls=[]
+   
+if blue_ball_n(balls) == max_balls_number:
+    max_in_n_balls.append("blue")
+if yellow_ball_n(balls) == max_balls_number:
+    max_in_n_balls.append("yellow")
+if red_ball_n(balls) == max_balls_number:
+    max_in_n_balls.append("red")
+if orange_ball_n(balls) == max_balls_number:
+    max_in_n_balls.append("orange")   
+    
+if blue_ball_n(balls) == min_balls_number:
+    min_in_n_balls.append("blue")
+if yellow_ball_n(balls) == min_balls_number:
+    min_in_n_balls.append("yellow")
+if red_ball_n(balls) == min_balls_number:
+    min_in_n_balls.append("red")
+if orange_ball_n(balls) == min_balls_number:
+    min_in_n_balls.append("orange")
+
+print("最多球的顏色",max_in_n_balls)   
+
+print("最少球的顏色",min_in_n_balls) 
+
+print("平均一個色多少球",count_balls(balls)/n_colors(balls))
+
+print("{顏色：數量}",n_balls)
+
+print(sorted(n_balls.keys()))
+#我以為他會由大到小排,結果很奇怪
