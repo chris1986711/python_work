5b159ad55d77e49c8dcb7d5889e45485bd539c50
chris0711 <chris1986.711@gmail.com>
2021-11-21 12:00:22 +0800

    mad_libs_game

---
+++ Mad_libs_game.py
@@ -0,0 +1,8 @@
+###         Mad libs game          ####
+age = input("Enter your age: ")
+high = input("enter your hight: ")
+weight = input("Enter your weight: ")
+
+print("My age", age)
+print(high+ " tall")
+print("my weight", weight)
