8c51d6471f2b02e5c78365f7a446254a5ff782f3
chris0711 <chris1986.711@gmail.com>
2021-12-14 19:59:23 +0800

    完善” “

--- buy_or_rent.py
+++ buy_or_rent.py
@@ -6,19 +6,20 @@ house_price=input("總價：")
 furnish=input("裝修款：")
 d=int(down_payment)
 p=int(house_price)
-f=int(furnish)
+bf=int(furnish)
 
-sum_buy_once=d+p+f
+prepare_fund=d
+
 ###每個月產生###
 
 interest=input("利息：")
 commute_buy=input("每月通勤距離km:")
 principal_repayment=input("每月還本金：")
 i=int(interest)
-cb=int(commute_buy)*3#每公里平均3塊
+cb=int(commute_buy)*60#每公里平均3塊每月20天
 pr=int(principal_repayment)
 
-sum_buy_monthly=i+cb
+buy_comsupmpyion=i+cb
 
 ####租房####
 ###每個月產生###
@@ -27,25 +28,25 @@ rent=input("每月租金:")
 commute_rent=input("每月通勤距離：")
 
 r=int(rent)
-cr=int(commute_rent)*3#每公里平均3塊
+cr=int(commute_rent)*60#同cb
 
-sum_rent_monthly=r+cr
+rent_comsumption=r+cr
 
 ####比較####
 
-compare_time=input("打算住幾年:")
-average_time_year=7
-
-sum_buy_avg=sum_buy_monthly*average_time_year*12
-sum_rent_avg=sum_rent_monthly*average_time_year*12
-
-print("買房消費:",sum_buy_avg,"租房消費:",sum_rent_avg)
-
-if sum_buy_avg > sum_rent_avg:
-    print("買<租")
-else:
-    print("買>租")
-
-already_pay=d+pr*average_time_year*12
+compare_time=10
+bc=buy_comsupmpyion*compare_time*12
+rc=rent_comsumption*compare_time*12
+bpm=buy_comsupmpyion+pr
+rpm=rent_comsumption
+already_pay=d+pr*compare_time*12
 debt=p-already_pay
-print("已付房款:",already_pay,"剩餘房款:",debt)
+
+
+print("買房消費:",bc,"租房消費:",rc)
+if bc > rc:
+    print("租比較好")
+else:
+    print("買比較好,需準備",prepare_fund)
+print("買房每月需繳：",bpm,"租房每月需繳：",rpm)
+print("資產:",already_pay,"剩餘房貸:",debt)
