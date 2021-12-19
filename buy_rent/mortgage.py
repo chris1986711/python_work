2b241dfe93241c97f62ca88bfe066dec346bcdfa
chris0711 <chris1986.711@gmail.com>
2021-12-19 18:11:10 +0800

    total

--- buy_rent/mortgage.py
+++ buy_rent/mortgage.py
@@ -11,15 +11,21 @@ rm=ry/12#月息＝年息/12
 loan_year=input("貸款年數：")
 ly=int(loan_year)
 lm=ly*12#貸款月數
-repayment_principal_month=p//lm
+repayment_principal_month=p/lm
 #利息表
 i=0
 interest_list=[]
+payment_month=[]
 while i <= lm:
     im=p*(lm-i)//lm*rm
-    im1=int(im)
+    im1=round(im)
+    im2=round(im1+repayment_principal_month)
     interest_list.append(im1)
+    payment_month.append(im2)
     i=i+1
-print(interest_list)
-##def 每月利息into list##
+total_interest=sum(interest_list)
+total_payment=sum(payment_month)
+average_payment=round(total_payment/lm)
+##每月本息還款##
 
+print("總還款：",total_payment,"總利息：",total_interest,"平均每月還：",average_payment)
