a834f7a502fc359bbba6cc97465c4315e5da9bc9
chris0711 <chris1986.711@gmail.com>
2021-12-20 21:56:26 +0800

    testvel

--- leetcode/Remove_Duplicates_from_Sorted_Array.py
+++ leetcode/Remove_Duplicates_from_Sorted_Array.py
@@ -3,19 +3,20 @@ def rd(n:list):
     k=len(n)
     i=0
     j=0
-    n2=[None]*k
+    n2=[]
     while i <=k-1:
         if n[i]==n[k-1]:
-            n2[j]=n[i]
+            n2.append(n[i])
             break
         elif n[i]==n[i+1]:
             i=i+1
         elif n[i]!=[i+1]:
-            n2[j]=n[i]
+            n2.append(n[i])
             i=i+1
-            j=j+1
+            j=j+1        
     return n2
 
 p=rd(nums)
 print(p)
+print(len(p))
         
