39d7ed34eb3662b08c1e5bd59c5ef57b098e4a6a
chris0711 <chris1986.711@gmail.com>
2021-12-21 21:46:43 +0800

    power_of_two

---
+++ leetcode/power_of_two.py
@@ -0,0 +1,14 @@
+
+def power(b:int):
+    j=0
+    while 2**j<=b:
+        if 2**j==b:
+            return True
+        elif 2**j<b:
+            j+=1
+    return False
+
+
+n=input()
+n=int(n)
+print(power(n))
