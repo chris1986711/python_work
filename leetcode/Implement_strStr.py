9545e1e398f244fb6d75ad9ecf72a64ae31fc25a
chris0711 <chris1986.711@gmail.com>
2021-12-22 12:55:04 +0800

    try

---
+++ leetcode/Implement_strStr.py
@@ -0,0 +1,24 @@
+a="hello"
+b="ll"
+a_list=list(a)
+b_list=list(b)
+k=len(a_list)-1
+l=len(b_list)-1
+i=0
+j=0
+if b in a:
+    print(True)
+    while i<=k:
+        if a_list[i]==b_list[0]:
+            
+            break
+        elif a_list!=b_list[0]:
+            i=i+1
+    print(i)
+else:
+    print(-1)
+    print(False)
+
+    
+
+
