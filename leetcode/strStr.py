4c49bda272e112ba960b9f296bd36ae46a5d30e6
chris0711 <chris1986.711@gmail.com>
2021-12-23 16:53:47 +0800

    rename

--- leetcode/strStr.py
+++ leetcode/strStr.py
@@ -1,32 +1,26 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return (0)
         h_list=list(haystack)
         n_list=list(needle)
         k=len(h_list)-1
         l=len(n_list)-1
         i=0
         j=0
-        if needle in haystack and n_list !=[]:
-            while i<=k:
-                if h_list[i]==n_list[j]:
-                    while j<=l and i<=k:
-                        if h_list[i]==n_list[j]:
-                            i=i+1
-                            j=j+1
-                        elif h_list[i]!=n_list[j]:
-                            i=i+1
-                            j=0
-                    break       
-                elif h_list!=n_list[j]:
-                    i=i+1
-            return(i-len(n_list))
-        elif needle in haystack and n_list==[]:
-            return (0)
-        else: 
-            return(-1)
+        star=0
+        while i<=k and j<=l:
+            if h_list[i]==n_list[j]:
+                if j==l:
+                    return star
+                i=i+1
+                j=j+1
+            else:
+                star=star+1
+                i=star
+                j=0
+        return -1
 
 
-"mississippi"
-"issip"
 x=Solution.strStr(self=Solution,haystack="mississippi",needle="issip")
 print(x)
